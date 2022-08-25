145885868738cb9696002c5d22526efee5d00d65
hjelev <jeleff@gmail.com>
2022-08-25 19:11:31 +0300

    Readme update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cv
 check latest software version
+
+## Supported software
+
+# Github
+
+aristocratos/btop
+hashicorp/terraform
+
+# Everything listed on https://endoflife.date
--- cv.py
+++ cv.py
@@ -9,7 +9,7 @@ endoflife_url = "https://endoflife.date/api/{}.json"
 def check_eoflife(name):
     with urllib.request.urlopen(endoflife_url.format(name)) as url:
         data = json.loads(url.read().decode())
-        if len(sys.argv) >1:
+        if table:
             print("|{:<10}|{:<10}|{:<10}|".format(name, data[0]['latest'],data[0]['releaseDate']))
         else:
             print("{:<10} {:<10} {:<10}".format(name, data[0]['latest'],data[0]['releaseDate']))
@@ -17,19 +17,32 @@ def check_eoflife(name):
 def check_github(name):
     with urllib.request.urlopen(github_url.format(name)) as url:
         data = json.loads(url.read().decode())
-        if len(sys.argv) >1:
+        if table:
             print("|{:<10}|{:<10}|{:<10}|".format(name.split('/')[1], data['name'].split('v')[1], data['created_at'].split('T')[0]))
         else:
             print("{:<10} {:<10} {:<10}".format(name.split('/')[1], data['name'].split('v')[1], data['created_at'].split('T')[0]))
  
 def check_versions(names):
-    for name in names:
-        if "/" in name:
-            check_github(name)
+    if isinstance(names, str):
+        if "/" in names:
+            check_github(names)
         else:
-            check_eoflife(name)
- 
-if len(sys.argv) >1 :
+            check_eoflife(names)
+    else:
+        for name in names:
+            if "/" in name:
+                check_github(name)
+            else:
+                check_eoflife(name)
+            
+if len(sys.argv) >1 and '-' in sys.argv[1] : 
+    table = True
+else:
+    table = False
+
+if len(sys.argv) >1 and '-' not in sys.argv[1] :
+    names = sys.argv[1]
+if table :
     print("||{:<10}||{:<10}||{:<10}||".format("Name", "Version", "Release Date"))
 else:   
     print("{:<10} {:<10} {:<10}\n".format("Name", "Version", "Release Date"))
